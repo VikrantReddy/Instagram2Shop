e7c85375c8de06a33ac18166a7b65a738fe31fd1
Vikrant Reddy <vikrantreddy@hotmail.com>
2021-08-21 12:13:43 +0530

    Added basic flask setup

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from app.mod_auth.controllers import mod_auth as auth_module
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+
+app.register_blueprint(auth_module)
+
+db.create_all()
